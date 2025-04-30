b8c147d0bd47372abf4dc060b0bcfb4b17d3c98e
ndugdale <67626400+ndugdale@users.noreply.github.com>
2025-04-30 23:31:26 +1000

    fix: remove unnecessary gh-pages files

--- src/core/dependencies.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "core/entity.h"
-#include "core/window.h"
-#include "renderer/renderer.h"
-
-EntityManager* get_entity_manager(void* dependencies);
-Renderer* get_renderer(void* dependencies);
-Window* get_window(void* dependencies);
